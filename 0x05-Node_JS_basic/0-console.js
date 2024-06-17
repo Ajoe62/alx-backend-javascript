1738cebea43373f7de2a81130febf142a77a960e
Ajoe62 <ijeborjoe@gmail.com>
2024-06-17 19:36:36 +0000

    task 0

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,4 @@
-
-function displayMessage(message){
-	console.log(message)
-};
-
-module.exports = displayMessage
+function displayMessage(message) {
+  console.log(message);
+}
+module.exports = displayMessage;
